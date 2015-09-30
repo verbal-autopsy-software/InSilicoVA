68ae3a5bc3fe8e00152fc97124e077fe000699c7
richardli <lizehang@gmail.com>
2015-09-30 16:25:36 -0700

    update documentation

--- InSilicoVA/R/report.r
+++ InSilicoVA/R/report.r
@@ -14,19 +14,17 @@
 #' cause of death distribution will be saved to the file.
 #' @param top Number of top causes to display on screen.
 #' @param \dots Not used.
-#' @return \item{id}{ the ID of the deaths.} \item{indiv}{ individual Cause of
-#' Death distribution matrix.} \item{csmf}{ CSMF distribution and confidence
-#' interval for each cause.} \item{csmf.ordered}{ CSMF distribution and
-#' confidence interval for each cause, ordered by mean.} \item{condprob}{
-#' Conditional probability matrix and confidence intervals.}
-#' 
+#' @return \item{id}{ the ID of the deaths.} 
+#' \item{indiv}{ individual Cause of Death distribution matrix.} 
+#' \item{csmf}{ CSMF distribution and confidence interval for each cause.} 
+#' \item{csmf.ordered}{ CSMF distribution and confidence interval for each cause, ordered by mean.} 
+#' \item{condprob}{ Conditional probability matrix and confidence intervals.}
 #' \item{useProbbase}{Component of \code{"insilico"} object.}
 #' \item{keepProbbase.level}{Component of \code{"insilico"} object.}
 #' \item{datacheck}{Component of \code{"insilico"} object.}
 #' \item{length.sim}{Component of \code{"insilico"} object.}
 #' \item{thin}{Component of \code{"insilico"} object.} \item{burnin}{Component
-#' of \code{"insilico"} object.} % \item{HIV}{Component of \code{"insilico"}
-#' object.} % \item{Malaria}{Component of \code{"insilico"} object.}
+#' of \code{"insilico"} object.} 
 #' \item{jump.scale}{Component of \code{"insilico"} object.}
 #' \item{levels.prior}{Component of \code{"insilico"} object.}
 #' \item{levels.strength}{Component of \code{"insilico"} object.}
@@ -176,75 +174,3 @@ summary.insilico <- function(object, CI.csmf = 0.95, CI.cond = 0.95,
 	class(out) <- "insilico_summary"
 	return(out)
 }
-
-print.insilico <- function(x,...){
-	cat("InSilicoVA fitted object:\n")
-	cat(paste(length(x$id), "death processed\n"))
-	cat(paste(x$length.sim, "iterations performed, with first", 
-			  x$burnin, "iterations discarded\n",
-			  trunc((x$length.sim - x$burnin)/x$thin), "iterations saved after thinning\n"))
-		if(x$useProbbase){
-			cat("Fitted with InterVA4 conditional probability matrix\n")
-		}else if(x$keepProbbase.level){
-			cat("Fitted with re-estimated InterVA4 conditional probability level table\n")	
-		}else{
-			cat("Fitted with re-estimating InterVA4 conditional probability matrix\n")	
-		}    
-}
-
-print.insilico_summary <- function(x, ...) {
-	cat("InSilicoVA Call: \n")
-	cat(paste(length(x$id), "death processed\n"))
-	cat(paste(x$length.sim, "iterations performed, with first", 
-			  x$burnin, "iterations discarded\n",
-			  trunc((x$length.sim - x$burnin)/x$thin), "iterations saved after thinning\n"))
-	if(x$useProbbase){
-			cat("Fitted with InterVA4 conditional probability matrix\n")
-		}else if(x$keepProbbase.level){
-			cat("Fitted with re-estimated InterVA4 conditional probability level table\n")	
-		}else{
-			cat("Fitted with re-estimating InterVA4 conditional probability matrix\n")	
-		}   
-	if(x$datacheck){
-		cat("Data consistency check performed as in InterVA4 \n")
-	}
-	if(!is.null(x$HIV)){
-		cat(paste("HIV level:     ", x$HIV, "\n"))
-		cat(paste("Malaria level: ", x$Malaria, "\n"))
-	}
-
-	if(!is.null(x$subpop_counts)){
-		cat("Sub population frequencies:")
-		print(x$subpop_counts)
-	}	
-	
-	if(class(x$csmf.ordered) != "list"){
-		top <- min(x$showTop, dim(x$csmf.ordered)[1])
-		cat("\n")
-		cat(paste("Top", top,  "CSMFs:\n"))
-		csmf.out.ordered <- x$csmf.ordered
-	    out <- as.matrix(csmf.out.ordered[1:top, ])
-			out[, 1] <- round(out[, 1], 4)
-			out[, 2] <- round(out[, 2], 4)
-			out[, 3] <- round(out[, 3], 4)
-			out[, 4] <- round(out[, 4], 4)
-			out[, 5] <- round(out[, 5], 4)
-		print(out)		
-	}else{
-		for(i in 1:length(x$csmf.ordered)){
-			top <- min(x$showTop, dim(x$csmf.ordered[[i]])[1])
-			cat("\n")
-			cat(paste(names(x$csmf.ordered)[i], "- Top", top,  "CSMFs:\n"))
-			csmf.out.ordered <- x$csmf.ordered[[i]]
-		    out <- as.matrix(csmf.out.ordered[1:top, ])
-				out[, 1] <- round(out[, 1], 4)
-				out[, 2] <- round(out[, 2], 4)
-				out[, 3] <- round(out[, 3], 4)
-				out[, 4] <- round(out[, 4], 4)
-				out[, 5] <- round(out[, 5], 4)
-			print(out)		
-		}
-	}
-}
-	
-

06bfcd6a6284cece7a031a747aaedbef0c3a3af0
richardli <lizehang@gmail.com>
2015-10-08 12:40:29 +0100

    Rcheck folder deleted

--- InSilicoVA.Rcheck/00_pkg_src/InSilicoVA/R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function( lib, pkg ) {
-   packageStartupMessage(
-      paste0( "\nPlease cite the 'InSilicoVA' package as:\n",
-         "Tyler H. McCormick, Zehang R. Li, Clara Calvert, Amelia C. Crampin, Kathleen Kahn and Samuel J. Clark (2014). ",
-         "Probabilistic cause-of-death assignment using verbal autopsies, ",
-         "arXiv preprint arXiv:1411.3042\n"),
-      domain = NULL,  appendLF = TRUE )
-}

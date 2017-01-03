5ac2fac036e7ec2f024d02fa206a872cb8c0da05
richardli <lizehang@gmail.com>
2017-01-03 00:55:44 -0800

    fix typo in document and update citation

--- InSilicoVA/R/zzz.R
+++ InSilicoVA/R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function( lib, pkg ) {
    packageStartupMessage(
       paste0( "\nPlease cite the 'InSilicoVA' package as:\n",
-         "Tyler H. McCormick, Zehang R. Li, Clara Calvert, Amelia C. Crampin, Kathleen Kahn and Samuel J. Clark (2014). ",
+         "Tyler H. McCormick, Zehang R. Li, Clara Calvert, Amelia C. Crampin, Kathleen Kahn and Samuel J. Clark (2016). ",
          "Probabilistic cause-of-death assignment using verbal autopsies, ",
-         "Journal of the American Statistical Association, to appear\n"),
+         "Journal of the American Statistical Association, to appear.\n"),
       domain = NULL,  appendLF = TRUE )
 }
